f41dbb627ee43dde6c2475c261cd5b6636a11cf3
Karim Elsayed <112348294+KarimElsayed10@users.noreply.github.com>
2024-02-07 02:31:08 +0200

    Add files via upload

---
+++ Double Linked List/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <doubleLinkedList.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    doubleLinkedList linked;
+    linked.insertLift(4);
+    linked.insertRight(56);
+    linked.insertAtPosition(1,12);
+    linked.insertLift(78);
+    linked.insertRight(66);
+    linked.insertAtPosition(1,36);
+    linked.insertLift(2);
+    linked.insertRight(85);
+    linked.insertAtPosition(1,32);
+    linked.insertLift(95);
+    linked.insertLift(59);
+    linked.print();
+    linked.removeAtPosition(4);
+    linked.removeLift();
+    linked.removeRight();
+    linked.print();
+    linked.reverse();
+    linked.print();
+    linked.sort();
+    linked.print();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
